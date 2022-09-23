61c00f1b10025155ed55b48c1f2333eea05650d2
rmney <riyadati.rm@gmail.com>
2022-09-23 20:18:43 +0100

    ex00 done

--- c00/ex01/phonebook.cpp
+++ c00/ex01/phonebook.cpp
@@ -3,19 +3,40 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.cpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 04:16:07 by rimney            #+#    #+#             */
-/*   Updated: 2022/09/21 22:50:30 by rimney           ###   ########.fr       */
+/*   Updated: 2022/09/23 07:00:36 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "phonebook.hpp"
 
+
+void  phonebook::ft_display_prompt(void)
+{
+    Contact C;
+    while(1)
+    {
+        std::string input;
+        std::cout << "Hi Plase Enter One Of This Commands > ADD, SEARCH, EXIT : ";
+        std::cin >> input;
+        if(input == "ADD")
+            add_number();
+        if(input == "SEARCH")
+            search();
+        if(input == "EXIT")
+            break ;
+    }
+}
+
+
+
+
+
 int main()
 {
     phonebook p;
-
-    p.add_number();
-    p.display_number();
+    p.ft_display_prompt();
+    // p.add_number();
 }
--- c00/ex01/phonebook.hpp
+++ c00/ex01/phonebook.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   phonebook.hpp                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
+/*   By: rimney < rimney@student.1337.ma>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 03:46:11 by rimney            #+#    #+#             */
-/*   Updated: 2022/09/21 23:35:48 by rimney           ###   ########.fr       */
+/*   Updated: 2022/09/23 07:36:14 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ class Contact
 {
     public :
         void ft_add_contact(int index);
-        void ft_display_contact(void);
+        void search_number(void);
     private :
         std::string first_name;
         std::string last_name;
@@ -30,6 +30,27 @@ class Contact
         int         index;
 };
 
+class phonebook
+{
+    public :
+        Contact t[8];
+        int index = 0;
+        void ft_display_prompt();
+        void    search(void);
+        void add_number();
+        void    display_number();
+        void    exit_program();
+};
+
+
+
+void Contact::search_number()
+{
+    std::cout << "Index     |First Name|Last Name |Nick name |" << std::endl;
+    
+    std::cout << Contact::index;
+    
+}      
 void Contact::ft_add_contact(int index)
 {
     std::cout << "Please Enter Fisrt Name : " << std::endl;
@@ -41,23 +62,39 @@ void Contact::ft_add_contact(int index)
     std::cin >> Contact::phone_number;
     std::cout << "Please Enter Nick Name : " << std::endl;
     std::cin >> Contact::nick_name;
+    search_number();
 }
 
-void Contact::ft_display_contact()
-{
-    std::cout << "Index     |First Name|Last Name |Nick name |" << std::endl;
-    std::cout << Contact::index;
-}           
+     
 
 
-class phonebook
+
+void phonebook::search()
 {
-    public :
-        Contact t[8];
-        int index = 0;
-        void add_number();
-        void    display_number();
-};
+    int i = 0;
+    std::string input;
+    while(1)
+    {
+        if(index >= 1)
+        {
+            std::cout << "|----------|----------|----------|----------|\n";
+            std::cout << "|index     |First Name|Last Name |Nickname  |\n";
+            while(i < index)
+            {
+                std::cout << "|----------|----------|----------|----------|\n";
+                std::cout << "|" << index << std::setw(11) << "|" << t[index].first_name << "\n";
+                i++;
+            }
+            break;
+        }
+        else
+        {
+            std::cout << "not ARGS\n";
+            break;
+        }
+    }
+    
+}
 
 void phonebook::add_number()
 {
@@ -66,14 +103,14 @@ void phonebook::add_number()
     index_save = index + 1;
     if (index < 8)
         t[index++].ft_add_contact(index_save);
+    std::cout << index_save << "index << \n" ;
     index %= 8;
 }
 
 void phonebook::display_number()
 {
     for (int i = 0; i < index; i++)
-        t[i].ft_display_contact();
-    
+        t[i].search_number();
 }
 
 #endif
