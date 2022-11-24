4689050a35695ca76ef323ae2d70f61955684ba6
Riyad Mney <rimney@e2r10p8.1337.ma>
2022-11-24 04:49:39 +0100

    rr

--- c05/ex02/main.cpp
+++ c05/ex02/main.cpp
@@ -6,19 +6,20 @@
 /*   By: rimney <rimney@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 03:34:24 by rimney            #+#    #+#             */
-/*   Updated: 2022/11/23 23:32:33 by rimney           ###   ########.fr       */
+/*   Updated: 2022/11/24 04:34:36 by rimney           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 #include "Form.hpp"
 #include "PresidentialPardonForm.hpp"
+#include "ShrubberyCreationForm.hpp"
 
 int main()
 {
     try
     {
-        PresidentialPardonForm f("home",  25, 5); // gotta set a param constructor that sets data to the form pv grade >>>> !
+        ShrubberyCreationForm f("home",  25, 5); // gotta set a param constructor that sets data to the form pv grade >>>> !
         Bureaucrat b("bureaucrat", 25);
         std::cout << f.getEGrade() << std::endl;
         b.signForm(f);
